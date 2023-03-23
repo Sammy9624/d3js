ebae720b119ef53747eda529efe7f3e6602dff2d
Rajiv <ciphernutz9624@gmail.com>
2023-03-23 09:26:15 +0530

    added cx and cy function

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 async function draw() {
   //data
   const dataset = await d3.json("data.json");
-  xAccessor = (d) => d.currently.humidity;
-  yAccessor = (d) => d.currently.apparentTemperature;
+  const xAccessor = (d) => d.currently.humidity;
+  const yAccessor = (d) => d.currently.apparentTemperature;
+
   let dimensions = {
     width: 800,
     height: 800,
@@ -13,6 +14,10 @@ async function draw() {
       bottom: 50,
     },
   };
+  dimensions.ctrHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+  dimensions.ctrWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
   //drawing image
   const svg = d3
     .select("#chart")
@@ -26,12 +31,24 @@ async function draw() {
       "transform",
       `translate(${dimensions.margin.left},${dimensions.margin.top})`
     );
+
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .rangeRound([0, dimensions.ctrWidth])
+    .clamp(true);
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .rangeRound([0, dimensions.ctrHeight])
+    .nice()
+    .clamp(true);
   ctr
     .selectAll("circle")
     .data(dataset)
     .join("circle")
-    .attr("cx", xAccessor)
-    .attr("cy", yAccessor)
+    .attr("cx", (d) => xScale(xAccessor(d)))
+    .attr("cy", (d) => yScale(yAccessor(d)))
     .attr("r", 5)
     .attr("fill", "red");
   // ctr.append("circle").attr("r", 15);
